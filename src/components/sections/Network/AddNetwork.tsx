693469123ec0062a087f2b676e7e36bbb12dfc24
bluecco <bluecco@gmail.com>
2024-11-01 16:17:21 +0100

    feat: network section

---
+++ src/components/sections/Network/AddNetwork.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/Button"
+import { Flex } from "@/components/Flex"
+import { ETHTokenAddress } from "@/constants"
+import { useWalletRequest } from "@starknet-react/core"
+import { shortString } from "starknet"
+
+const AddNetwork = () => {
+  const walletRequest = useWalletRequest({
+    type: "wallet_addStarknetChain",
+    params: {
+      id: "ZORG",
+      chain_id: shortString.encodeShortString("ZORG"), // A 0x-prefixed hexadecimal string
+      chain_name: "ZORG",
+      rpc_urls: ["http://192.168.1.44:6060"],
+      native_currency: {
+        type: "ERC20",
+        options: {
+          address: ETHTokenAddress, // Not part of the standard, but required by StarkNet as it can work with any ERC20 token as the fee token
+          name: "ETHEREUM",
+          symbol: "ETH", // 2-6 characters long
+          decimals: 18,
+        },
+      },
+    },
+  })
+
+  return (
+    <Flex
+      color="black"
+      borderWidth="0px"
+      borderRadius="8px"
+      justifyContent="flex-start"
+      width="100%"
+    >
+      <Button
+        onClick={async () => {
+          await walletRequest.requestAsync()
+        }}
+        style={{ width: "100%" }}
+      >
+        Add Network
+      </Button>
+    </Flex>
+  )
+}
+
+export { AddNetwork }
---
+++ src/components/sections/Network/ChangeNetwork.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/Button"
+import { Flex } from "@/components/Flex"
+import { useWalletRequest } from "@starknet-react/core"
+import { constants } from "starknet"
+
+const ChangeNetwork = () => {
+  const walletRequest = useWalletRequest({
+    type: "wallet_switchStarknetChain",
+    params: {
+      chainId: constants.StarknetChainId.SN_MAIN,
+    },
+  })
+
+  return (
+    <Flex
+      color="black"
+      borderWidth="0px"
+      borderRadius="8px"
+      justifyContent="flex-start"
+      width="100%"
+    >
+      <Button
+        onClick={async () => {
+          await walletRequest.requestAsync()
+        }}
+        style={{ width: "100%" }}
+      >
+        Change Network
+      </Button>
+    </Flex>
+  )
+}
+
+export { ChangeNetwork }
